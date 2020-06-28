19057acb5306a28b98d53e22a2847e0f0c9ba8a3
MetaSeth <alexandre.mokni@gmail.com>
2020-06-28 03:17:46 +0200

    Camera initialisation

---
+++ cyber_scanner/lib/camera_preview_scanner.dart
@@ -0,0 +1,71 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class CameraPreviewScanner extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _CameraPreviewScannerState();
+}
+
+class _CameraPreviewScannerState extends State<CameraPreviewScanner> {
+  dynamic _scanResults;
+  CameraController _camera;
+
+  bool _isDetecting = false;
+  CameraLensDirection _direction = CameraLensDirection.back;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeCamera();
+  }
+
+  void _initializeCamera() async {
+    final CameraDescription description = await availableCameras().then(
+      (List<CameraDescription> cameras) => cameras.firstWhere(
+        (CameraDescription camera) => camera.lensDirection == _direction,
+      ),
+    );
+
+    _camera = CameraController(
+      description,
+      defaultTargetPlatform == TargetPlatform.android
+          ? ResolutionPreset.high
+          : ResolutionPreset.low,
+    );
+    await _camera.initialize();
+
+    _camera.startImageStream((CameraImage image) {
+      if (_isDetecting) return;
+
+      _isDetecting = true;
+    });
+  }
+
+  Widget buildImage() {
+    return Container(
+      child: _camera == null
+          ? const Center(
+              child: Text(
+                'Initializing Camera...',
+                style: TextStyle(
+                  color: Colors.green,
+                  fontSize: 30.0,
+                ),
+              ),
+            )
+          : Stack(
+              fit: StackFit.expand,
+              children: <Widget>[
+                CameraPreview(_camera),
+                // _buildResults(),
+              ],
+            ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return buildImage();
+  }
+}
